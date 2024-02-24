6c94f9cd44e6a13728447b3980b165c86f1bdf5d
Vmjlol <maniezzojr@yahoo.com.br>
2024-02-23 21:06:04 -0300

    outra mudança kk

--- script.js
+++ script.js
@@ -91,11 +91,38 @@ let ex09 = () => {
     let number = +document.getElementById('number').value;
     let reply = document.getElementById('jsReply');
 
+    reply.innerText = "";
+
     for (let i = 1; i <= 10; i++) {
         reply.innerText += `${number} x ${i}: ${number * i}\n`;
     }
 }
 
 let ex10 = () => {
+    let getName = document.getElementById('name').value;
+    let getEmail = document.getElementById('email').value;
+    let emailTest = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+    let getTel = document.getElementById('cel').value;
+
+    if (getName == '') {
+        document.getElementById('name').placeholder = 'Erro, nome em branco';
+    }
+    
+    if (getTel == ''){
+        document.getElementById('cel').placeholder = 'Erro, telefone em branco';
+    }
+
+    if(getEmail == '') { 
+        document.getElementById('email').placeholder = 'Erro, email vazio';
+        return false; 
+    }
+
+    if(getEmail !== '' && !emailTest.test(getEmail)) { 
+        alert('E-mail não preenchido adequadamente');
+    }
+
+    if (getName !== '' && emailTest.test(getEmail) && getTel !== '') {
+        alert('Enviado com sucesso')
+    }
 
 }
